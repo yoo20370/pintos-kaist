348f4f3734f62c469d50e5815886a2b0677cf364
6puritans9 <6puritans9@gmail.com>
2024-05-12 22:56:42 +0900

    implement initial alarm clock

--- include/devices/timer.h
+++ include/devices/timer.h
@@ -7,17 +7,25 @@
 /* Number of timer interrupts per second. */
 #define TIMER_FREQ 100
 
-void timer_init (void);
-void timer_calibrate (void);
+/* Converts pointer to list element LIST_ELEM into a pointer to
+   the structure that LIST_ELEM is embedded inside.  Supply the
+   name of the outer structure STRUCT and the member name MEMBER
+   of the list element.  See the big comment at the top of the
+   file for an example. */
+#define list_entry(LIST_ELEM, STRUCT, MEMBER) \
+    ((STRUCT *)((uint8_t *)&(LIST_ELEM)->next - offsetof(STRUCT, MEMBER.next)))
 
-int64_t timer_ticks (void);
-int64_t timer_elapsed (int64_t);
+void timer_init(void);
+void timer_calibrate(void);
 
-void timer_sleep (int64_t ticks);
-void timer_msleep (int64_t milliseconds);
-void timer_usleep (int64_t microseconds);
-void timer_nsleep (int64_t nanoseconds);
+int64_t timer_ticks(void);
+int64_t timer_elapsed(int64_t);
 
-void timer_print_stats (void);
+void timer_sleep(int64_t ticks);
+void timer_msleep(int64_t milliseconds);
+void timer_usleep(int64_t microseconds);
+void timer_nsleep(int64_t nanoseconds);
+
+void timer_print_stats(void);
 
 #endif /* devices/timer.h */
